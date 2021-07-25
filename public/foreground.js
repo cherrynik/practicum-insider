fdac75423525c9a968c15f456f94b043fa1828c9
Nikolay <ighosta9@gmail.com>
2021-07-25 12:13:26 +0000

    Finishing mechanics

--- public/foreground.js
+++
@@ -1,6 +0,0 @@
-alert("Hello!");
-// let forTranslation = document.querySelectorAll('[data-resource]');
-// forTranslation.forEach(function(el) {
-//     el.innerText =
-//         chrome.i18n.getMessage(el.getAttribute('data-resource'));
-// })
